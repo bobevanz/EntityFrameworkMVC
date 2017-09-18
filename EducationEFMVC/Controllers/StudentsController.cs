9b61a438e1a2895a294a26d9e33ea973cd987233
Bob <bobevans547@gmail.com>
2017-09-18 14:29:57 -0400

    EducationEFMVC

--- EducationEFMVC/Controllers/StudentsController.cs
+++ EducationEFMVC/Controllers/StudentsController.cs
@@ -46,7 +46,7 @@ namespace EducationEFMVC.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "id,FirstName,LastName,SAT,GPA")] Student student)
+        public ActionResult Create([Bind(Include = "id,FirstName,LastName,SAT,GPA,Phone,Email")] Student student)
         {
             if (ModelState.IsValid)
             {
@@ -78,7 +78,7 @@ namespace EducationEFMVC.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "id,FirstName,LastName,SAT,GPA")] Student student)
+        public ActionResult Edit([Bind(Include = "id,FirstName,LastName,SAT,GPA,Phone,Email")] Student student)
         {
             if (ModelState.IsValid)
             {
