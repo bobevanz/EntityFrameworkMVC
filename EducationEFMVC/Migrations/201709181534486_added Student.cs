3bd92d97f639ecb4713c316046bbb2be92158780
Bob <bobevans547@gmail.com>
2017-09-18 12:28:37 -0400

    Entity Framework MVC

---
+++ EducationEFMVC/Migrations/201709181534486_added Student.cs
@@ -0,0 +1,16 @@
+namespace EducationEFMVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addedStudent : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
